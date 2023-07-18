1659744b7b9f95380700a24670f7d991dc4b3c0a
Eric-rossWCCI <ericross@wecancodeit.org>
2023-07-18 08:50:30 -0400

    Code along notes

--- src/main/java/CollectionsDemo.java
+++ src/main/java/CollectionsDemo.java
@@ -8,7 +8,9 @@ import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.PriorityQueue;
+import java.util.Set;
 import java.util.TreeSet;
 
 import javax.print.attribute.HashAttributeSet;
@@ -21,78 +23,82 @@ public class CollectionsDemo {
     // create a method that iterates and loops through a list. Return list with
     // "Added" concatenated to every list item
 
-    public List<String> looping(){
+    public List<String> looping() {
         // create empty list of strings to house updated strings
         List<String> updatedWords = new ArrayList<>();
-        for(String s: words){
-                updatedWords.add(s+"Added");
+        for (String s : words) {
+            updatedWords.add(s + "Added");
         }
-        // use the built in foreach method to loop through items
+        // optionally you could use the built in foreach method to loop through items.
+        // uncomment the next line to try it out
         // words.forEach(word -> updatedWords.add(word + "Added"));
+
         // return list of updated words
         return updatedWords;
     }
 
-
-    public void iterating(){
-        // created new list from original list to remove direct backing of non resizable array
+    public void iterating() {
+        // created new list from original list to remove direct backing of non resizable
+        // array
         List<String> newWords = new ArrayList<>(words);
         // create iterator (java.util) to allow looping
-       Iterator<String> wordsIterator =  newWords.iterator();
+        Iterator<String> wordsIterator = newWords.iterator();
+        while (wordsIterator.hasNext()) {
+            // returns the next item in wordsIterator
+            String item = wordsIterator.next();
+            System.out.println(item);
 
-       wordsIterator.hasNext();
-        // returns the next method
-        String item = wordsIterator.next();
-        // System.out.println(item);
+            // removes the last item returned by next method
+            // this act of removing item from collection while looping is only possible
+            // through iterators
+            wordsIterator.remove();
+        }
 
-        // removes the last item returned by next method
-        wordsIterator.remove();
-       
     }
 
-
-    public void sets(){
+    public void sets() {
         // create a HashSet from the words collection
         HashSet<String> s = new HashSet<>(words);
-
+        // print to demonstrate duplication removal and loss of order
         System.out.println(s);
 
         // create linkedHashSet that preserves insertion order
         LinkedHashSet<String> ls = new LinkedHashSet<>(words);
         System.out.println("LinkedHashSet " + ls);
 
-        // create TreeSet 
+        // create TreeSet
         TreeSet<String> treeSet = new TreeSet<String>(words);
-        System.out.println("TreeSet "+ treeSet);
+        // print to demonstrate default natural ordering
+        System.out.println("TreeSet " + treeSet);
 
     }
 
-
-    public void queues(){
+    // queues are used implement FIFO structures
+    // PriorityQueues uses priority to determine who is next out instead of
+    // insertion order
+    public void queues() {
 
         // so let's use our collection instance variable to create a Priority
         PriorityQueue<String> priorityQueue = new PriorityQueue<>(words);
 
         // print and remove each item
-        System.out.println("Priority "+ priorityQueue.poll());
-        System.out.println("Priority "+ priorityQueue.poll());
-        System.out.println("Priority "+ priorityQueue.poll());
-        System.out.println("Priority "+ priorityQueue.poll());
-        System.out.println("Priority "+ priorityQueue.poll());
-        System.out.println("Priority "+ priorityQueue.poll());
-
+        System.out.println("Priority " + priorityQueue.poll());
+        System.out.println("Priority " + priorityQueue.poll());
+        System.out.println("Priority " + priorityQueue.poll());
+        System.out.println("Priority " + priorityQueue.poll());
+        System.out.println("Priority " + priorityQueue.poll());
+        System.out.println("Priority " + priorityQueue.poll());
 
         // create a linkedList from our words collection
         LinkedList<String> linkedList = new LinkedList<>(words);
 
         // while this list is not empty, iterate/loop again
-        while(!linkedList.isEmpty()){
+        while (!linkedList.isEmpty()) {
             System.out.println(linkedList.remove());
         }
     }
 
-
-    public void maps(){
+    public void maps() {
         // create a hashMap
         HashMap<Integer, Long> speedDial = new HashMap<Integer, Long>();
 
@@ -101,7 +107,56 @@ public class CollectionsDemo {
         System.out.println(speedDial);
         // retrieve the value tied to key 1
         System.out.println(speedDial.get(1));
-        
+
+        // Create a map that takes a String, List<String>
+        HashMap<String, List<String>> strMap = new HashMap<String, List<String>>();
+
+        strMap.put("commonWordCollection", words);
+        strMap.put("commonWordCollection2", words);
+        // demonstrate an attempt to retrieve an item from map that doesn't exist
+        System.out.println("key doesn't exist result " + strMap.get("string"));
+
+        // retrieve all keys from map using keySet
+        Set<String> keys = strMap.keySet();
+        // print keys
+        System.out.println(keys);
+
+        // loop through the values contained in a map
+        for (List<String> listOStrings : strMap.values()) {
+            listOStrings.contains("have");
+        }
+
+        // loop through a map using enhanced for loop
+        for (Map.Entry<String, List<String>> entry : strMap.entrySet()) {
+            String key = entry.getKey();
+            List<String> value = entry.getValue();
+            System.out.println("Key value pair: " + key + " " + value);
+        }
+
+        /*
+         * create method called returnMapString... return value whose length is
+         * less than 4. Return one string
+         */
+
+        /*
+         * exercise 2... return a concatenation of the key and value whose value length
+         * is less than 4
+         */
+
+        /*
+         * Return the key(s) that start with the letter M
+         */
+
+        /*
+         * Create class to represent contact info... i.e name, FAKE phone mumber, fake
+         * email address.
+         * Create user input for this state
+         * Add to contacts map. Key = Name and the value = contactInfo object.
+         * print out the contents of the map.
+         * 
+         * test the method that takes in the user input from the input method and
+         * creates the class and returns it.
+         */
 
     }
 }
